a0154696449c663df15c6ce9b696c34fae97b029
Makememyday <congthinhluong.new@gmail.com>
2025-01-30 16:57:11 +0700

    feat: map deploy

--- outernet/lib/presentation/screens/map/mapbox_implement.dart
+++ outernet/lib/presentation/screens/map/mapbox_implement.dart
@@ -1,27 +1,142 @@
-// import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+class FullMapPage {
+  FullMapPage();
 
-// import 'package:flutter/material.dart';
-// import 'package:outernet/env/mapbox_access_token.dart';
+  @override
+  Widget build(BuildContext context) {
+    return const FullMap();
+  }
+}
 
+class FullMap extends StatefulWidget {
+  const FullMap({super.key});
 
-// class MapboxMapWidget extends StatelessWidget {
-//   final String mapboxAccessToken = mapboxToken;
-  
-//   final CameraOptions camera = CameraOptions(
-//     center: Point(coordinates: Position(108.2203, 16.0471)),
-//     zoom: 10,
-//     bearing: 0,
-//     pitch: 0,
-//   );
-  
-//   MapboxMapWidget({super.key}) {
-//     MapboxOptions.setAccessToken(mapboxAccessToken);
-//   }
+  @override
+  State createState() => FullMapState();
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MapWidget(
-//       cameraOptions: camera,
-//     );
-//   }
-// }
+class FullMapState extends State<FullMap> {
+  MapboxMap? mapboxMap;
+  var isLight = true;
+
+  _onMapCreated(MapboxMap mapboxMap) {
+    this.mapboxMap = mapboxMap;
+    mapboxMap.style;
+  }
+
+  _onStyleLoadedCallback(StyleLoadedEventData data) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("Style loaded :), time: ${data.timeInterval}"),
+      backgroundColor: Theme.of(context).primaryColor,
+      duration: Duration(seconds: 1),
+    ));
+  }
+
+  _onCameraChangeListener(CameraChangedEventData data) {
+    print("CameraChangedEventData: timestamp: ${data.timestamp}");
+  }
+
+  _onResourceRequestListener(ResourceEventData data) {
+    print("ResourceEventData: time: ${data.timeInterval}");
+  }
+
+  _onMapIdleListener(MapIdleEventData data) {
+    print("MapIdleEventData: timestamp: ${data.timestamp}");
+  }
+
+  _onMapLoadedListener(MapLoadedEventData data) {
+    print("MapLoadedEventData: time: ${data.timeInterval}");
+  }
+
+  _onMapLoadingErrorListener(MapLoadingErrorEventData data) {
+    print("MapLoadingErrorEventData: timestamp: ${data.timestamp}");
+  }
+
+  _onRenderFrameStartedListener(RenderFrameStartedEventData data) {
+    print("RenderFrameStartedEventData: timestamp: ${data.timestamp}");
+  }
+
+  _onRenderFrameFinishedListener(RenderFrameFinishedEventData data) {
+    print("RenderFrameFinishedEventData: time: ${data.timeInterval}");
+  }
+
+  _onSourceAddedListener(SourceAddedEventData data) {
+    print("SourceAddedEventData: timestamp: ${data.timestamp}");
+  }
+
+  _onSourceDataLoadedListener(SourceDataLoadedEventData data) {
+    print("SourceDataLoadedEventData: time: ${data.timeInterval}");
+  }
+
+  _onSourceRemovedListener(SourceRemovedEventData data) {
+    print("SourceRemovedEventData: timestamp: ${data.timestamp}");
+  }
+
+  _onStyleDataLoadedListener(StyleDataLoadedEventData data) {
+    print("StyleDataLoadedEventData: time: ${data.timeInterval}");
+  }
+
+  _onStyleImageMissingListener(StyleImageMissingEventData data) {
+    print("StyleImageMissingEventData: timestamp: ${data.timestamp}");
+  }
+
+  _onStyleImageUnusedListener(StyleImageUnusedEventData data) {
+    print("StyleImageUnusedEventData: timestamp: ${data.timestamp}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FloatingActionButton(
+                  heroTag: null,
+                  onPressed: () {
+                    setState(
+                      () => isLight = !isLight,
+                    );
+                    if (isLight) {
+                      mapboxMap?.loadStyleURI(MapboxStyles.LIGHT);
+                    } else {
+                      mapboxMap?.loadStyleURI(MapboxStyles.DARK);
+                    }
+                  },
+                  child: Icon(Icons.swap_horiz)),
+              SizedBox(height: 10),
+            ],
+          ),
+        ),
+        body: MapWidget(
+          key: ValueKey("mapWidget"),
+          cameraOptions: CameraOptions(
+              center: Point(
+                  coordinates: Position(
+                6.0033416748046875,
+                43.70908256335716,
+              )),
+              zoom: 3.0),
+          styleUri: MapboxStyles.LIGHT,
+          textureView: true,
+          onMapCreated: _onMapCreated,
+          onStyleLoadedListener: _onStyleLoadedCallback,
+          onCameraChangeListener: _onCameraChangeListener,
+          onMapIdleListener: _onMapIdleListener,
+          onMapLoadedListener: _onMapLoadedListener,
+          onMapLoadErrorListener: _onMapLoadingErrorListener,
+          onRenderFrameStartedListener: _onRenderFrameStartedListener,
+          onRenderFrameFinishedListener: _onRenderFrameFinishedListener,
+          onSourceAddedListener: _onSourceAddedListener,
+          onSourceDataLoadedListener: _onSourceDataLoadedListener,
+          onSourceRemovedListener: _onSourceRemovedListener,
+          onStyleDataLoadedListener: _onStyleDataLoadedListener,
+          onStyleImageMissingListener: _onStyleImageMissingListener,
+          onStyleImageUnusedListener: _onStyleImageUnusedListener,
+          onResourceRequestListener: _onResourceRequestListener,
+          onLongTapListener: (coordinate) {},
+        ));
+  }
+}
